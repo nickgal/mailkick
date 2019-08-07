517635f9770da0a7ad7fff9bb531c0557e6559e6
Nick Gal <nickgal@me.com>
2019-08-07 14:01:53 -0700

    require so the constant isn't autoloaded

--- lib/mailkick/engine.rb
+++ lib/mailkick/engine.rb
@@ -7,6 +7,7 @@ module Mailkick
       secrets = app.respond_to?(:secrets) ? app.secrets : app.config
       Mailkick.secret_token ||= secrets.respond_to?(:secret_key_base) ? secrets.secret_key_base : secrets.secret_token
       ActiveSupport.on_load :action_mailer do
+        require 'mailkick/url_helper'
         helper Mailkick::UrlHelper
       end
     end
